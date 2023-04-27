7ee79509d49c7fdcb9b0c19adabc01d75944f2cb
Gukin <gukin.dev@gmail.com>
2023-04-27 12:55:09 +0900

    Refactor

--- src/main/java/com/example/findadjacentwifi/WifiLoadService.java
+++ src/main/java/com/example/findadjacentwifi/WifiLoadService.java
@@ -1,6 +1,5 @@
 package com.example.findadjacentwifi;
 
-import com.google.gson.JsonObject;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -13,20 +12,24 @@ import com.google.gson.JsonParser;
 import java.util.Arrays;
 import java.util.List;
 
-public class Service {
+public class WifiLoadService {
+    private String json;
+    private List<Wifi> wifiList;
 
-    public static void main(String[] args) {
-        Service service = new Service();
-        String json = service.getUserInfo("434665544267756b34325141564f69");
-        service.parseJSON(json);
-    }
+    private final String APIKEY = "434665544267756b34325141564f69";
 
-    public String getUserInfo(String key) {
+    public boolean getWifiAsJSON(int start, int end) {
         final String TYPE = "json";
 
-
         try {
-            String url = "http://openapi.seoul.go.kr:8088/"+ key +"/"+ TYPE + "/TbPublicWifiInfo/1/2/";
+            String startIdx = String.valueOf(start);
+            String endIdx = String.valueOf(end);
+            String url = "http://openapi.seoul.go.kr:8088/"
+                    + APIKEY + "/"
+                    + TYPE
+                    + "/TbPublicWifiInfo/"
+                    + startIdx + "/"
+                    + endIdx + "/";
 
             // OkHttp 클라이언트 객체 생성
             OkHttpClient client = new OkHttpClient();
@@ -41,35 +44,28 @@ public class Service {
                 // 응답 받아서 처리
                 ResponseBody body = response.body();
                 if (body != null) {
-//                    System.out.println(body.string());
-                    return body.string();
+                    this.json = body.string();
                 }
-            }
-            else
+            } else
                 System.err.println("Error Occurred");
 
-//            return true;
-        } catch(Exception e) {
+            return true;
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-//        return false;
-        return "";
+        return false;
     }
 
-
-
-    public void parseJSON(String json) {
+    public void parseJSON() {
 
         JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(json);
+        JsonElement element = parser.parse(this.json);
         JsonElement tbPublicWifiInfo = element.getAsJsonObject().get("TbPublicWifiInfo");
         JsonElement rows = tbPublicWifiInfo.getAsJsonObject().get("row");
 
         Gson gson = new Gson();
         Wifi[] array = gson.fromJson(rows, Wifi[].class);
-        List<Wifi> list = Arrays.asList(array);
-
+        this.wifiList = Arrays.asList(array);
     }
-
 }
