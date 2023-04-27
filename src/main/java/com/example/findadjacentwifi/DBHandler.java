190ca1ddb8255577bad849e98eee3344a2506795
Gukin <gukin.dev@gmail.com>
2023-04-27 17:46:27 +0900

    Add method droping table

--- src/main/java/com/example/findadjacentwifi/DBHandler.java
+++ src/main/java/com/example/findadjacentwifi/DBHandler.java
@@ -1,6 +1,7 @@
 package com.example.findadjacentwifi;
 
 import java.sql.*;
+import java.util.List;
 
 public class DBHandler {
     private static Connection conn;
@@ -40,11 +41,62 @@ public class DBHandler {
         }
     }
 
+    public static void insertWifi (Wifi wifi) {
+        String sql = "INSERT INTO " +
+                "wifi_unit " +
+                "(" +
+                "distance," +
+                "mgr_no," +
+                "wrdofc," +
+                "main_nm," +
+                "adres1," +
+                "arres2," +
+                "instl_floor," +
+                "instl_mby," +
+                "instal_ty," +
+                "svc_se," +
+                "cmcwr," +
+                "cnstc_year," +
+                "inout_door," +
+                "remars3," +
+                "lat," +
+                "lnt," +
+                "work_dttm" +
+                ") VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+        PreparedStatement pstmt;
+        try {
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setObject(1, null);
+            pstmt.setObject(2, wifi.getX_SWIFI_MGR_NO());
+            pstmt.setObject(3, wifi.getX_SWIFI_WRDOFC());
+            pstmt.setObject(4, wifi.getX_SWIFI_MAIN_NM());
+            pstmt.setObject(5, wifi.getX_SWIFI_ADRES1());
+            pstmt.setObject(6, wifi.getX_SWIFI_ADRES2());
+            pstmt.setObject(7, wifi.getX_SWIFI_INSTL_FLOOR());
+            pstmt.setObject(8, wifi.getX_SWIFI_INSTL_MBY());
+            pstmt.setObject(9, wifi.getX_SWIFI_INSTL_TY());
+            pstmt.setObject(10, wifi.getX_SWIFI_SVC_SE());
+            pstmt.setObject(11, wifi.getX_SWIFI_CMCWR());
+            pstmt.setObject(12, wifi.getX_SWIFI_CNSTC_YEAR());
+            pstmt.setObject(13, wifi.getX_SWIFI_INOUT_DOOR());
+            pstmt.setObject(14, wifi.getX_SWIFI_REMARS3());
+            pstmt.setObject(15, wifi.getLAT());
+            pstmt.setObject(16, wifi.getLNT());
+            pstmt.setObject(17, wifi.getWORK_DTTM());
+
+            pstmt.executeUpdate();
+            System.out.println("works!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     public static void creatTable() {
-        String sql = "create table if not exists wifi_unit " +
+        String sql = "create table if not exists wifi_unit" +
                 "(" +
                 "distance nummeric," +
+                "mgr_no text," +
                 "wrdofc text," +
                 "main_nm text," +
                 "adres1 text," +
@@ -72,9 +124,22 @@ public class DBHandler {
         }
     }
 
+    public static void dropTable() {
+        String sql = "drop table if exists wifi_unit";
+
+        Statement stmt = null;
+        try {
+            stmt = conn.createStatement();
+            stmt.execute(sql);
+            System.out.println("works!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         createConnection();
-        creatTable();
+        dropTable();
         closeConnection();
     }
 }
